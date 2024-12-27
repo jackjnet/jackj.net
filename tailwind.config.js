b8dcbfa944f9fb53a1714a719e2df5f70f1c4b0e
jackjnet <jwj@jackj.net>
2024-12-27 16:37:26 -0600

    add indieauth

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,html}"],
+  content: ["./index.html", "./src/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
         bskyblue: "#0085ff",
         githubgreen: "#2dba4e",
+        linkedinblue: "#0a66c2",
       },
     },
   },
